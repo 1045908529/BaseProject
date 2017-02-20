a7e98d8c64f8715364f9d72f1c78c17f568e9c7f
Tangjinghao <jhtang_colin@163.com>
2017-02-20 10:30:11 +0800

    0.1.0

--- BaseProject.podspec
+++ BaseProject.podspec
@@ -17,10 +17,9 @@ Pod::Spec.new do |s|
                        DESC
 
   s.homepage         = 'https://github.com/1045908529/BaseProject'
-  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { '1045908529' => 'jhtang_colin@163.com' }
-  s.source           = { :git => '/Users/admin/Documents/项目/BaseProject/BaseProject', :tag => 0.1.0 }
+  s.source           = { :git => 'https://github.com/1045908529/BaseProject.git', :tag => '0.1.0' }
 
   s.ios.deployment_target = '8.0'
 
@@ -28,13 +27,6 @@ Pod::Spec.new do |s|
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   s.frameworks = 'UIKit'
-  s.dependency 'AFNetworking', '~> 3.0.3'
   s.dependency 'BlocksKit', '~> 2.2.5'
   s.dependency 'MBProgressHUD'
-  s.dependency 'IQKeyboardManager'
-  s.dependency 'MBProgressHUD'
-  s.dependency 'Masonry', '~> 0.6.4'
-  s.dependency 'MagicalRecord', '~> 2.3.2'
-  s.dependency 'MJExtension', '~> 3.0.10'
-  s.dependency 'TMCache', '~> 2.1.0'
 end
--- Example/Pods/Target Support Files/Pods-BaseProject_Example/Pods-BaseProject_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-BaseProject_Example/Pods-BaseProject_Example-frameworks.sh
@@ -84,24 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/BaseProject/BaseProject.framework"
   install_framework "$BUILT_PRODUCTS_DIR/BlocksKit/BlocksKit.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/MJExtension/MJExtension.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/MagicalRecord/MagicalRecord.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/TMCache/TMCache.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/BaseProject/BaseProject.framework"
   install_framework "$BUILT_PRODUCTS_DIR/BlocksKit/BlocksKit.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/MJExtension/MJExtension.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/MagicalRecord/MagicalRecord.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/TMCache/TMCache.framework"
 fi
